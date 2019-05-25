06b14f1598d4af60d0da655a6a7508dff785c30e
Clivern <hello@clivern.com>
2019-05-25 23:14:59 +0200

    add shell command pkg

---
+++ pkg/shell.go
@@ -0,0 +1,39 @@
+// Copyright 2019 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package pkg
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+// Shell struct
+type Shell struct {
+}
+
+// Result struct
+type Result struct {
+	Stdout string
+	Stderr string
+}
+
+// Exec execute a command
+func (s *Shell) Exec(name string, arg ...string) (*Result, error) {
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command(name, arg...)
+
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+
+	if err != nil {
+		return &Result{}, err
+	}
+
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+
+	return &Result{Stdout: outStr, Stderr: errStr}, nil
+}
