0798f0e8fdeab6132a7f7afed1d9e08001ba8053
Clivern <hello@clivern.com>
2019-05-30 22:43:50 +0200

    support native broker

--- internal/app/controller/chan_release.go
+++ internal/app/controller/chan_release.go
@@ -5,13 +5,73 @@
 package controller
 
 import (
+	"github.com/clivern/rabbit/internal/app/module"
+	"github.com/clivern/rabbit/pkg"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 // ChanRelease controller
 func ChanRelease(c *gin.Context, messages chan<- string) {
-	messages <- "Hello"
+
+	var releaseRequest module.ReleaseRequest
+	validate := pkg.Validator{}
+
+	rawBody, err := c.GetRawData()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Invalid request",
+		})
+		return
+	}
+
+	ok, err := releaseRequest.LoadFromJSON(rawBody)
+
+	if !ok || err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Invalid request",
+		})
+		return
+	}
+
+	if validate.IsEmpty(releaseRequest.Name) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Repository name is required",
+		})
+		return
+	}
+
+	if validate.IsEmpty(releaseRequest.URL) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Repository url is required",
+		})
+		return
+	}
+
+	if validate.IsEmpty(releaseRequest.Version) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Repository version is required",
+		})
+		return
+	}
+
+	requestBody, err := releaseRequest.ConvertToJSON()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"error":  "Invalid request",
+		})
+		return
+	}
+
+	messages <- requestBody
 
 	c.Status(http.StatusAccepted)
 }
--- internal/app/controller/chan_worker.go
+++ internal/app/controller/chan_worker.go
@@ -5,13 +5,44 @@
 package controller
 
 import (
-	"fmt"
+	"github.com/clivern/rabbit/internal/app/module"
 )
 
 // ChanWorker runs async jobs
 func ChanWorker(messages <-chan string) {
 	for message := range messages {
-		fmt.Printf("Received: %s\n", message)
+		var releaseRequest module.ReleaseRequest
+
+		ok, err := releaseRequest.LoadFromJSON([]byte(message))
+
+		if !ok || err != nil {
+			continue
+		}
+
+		releaser, err := module.NewReleaser(
+			releaseRequest.Name,
+			releaseRequest.URL,
+			releaseRequest.Version,
+		)
+
+		if err != nil {
+			continue
+		}
+
+		_, err = releaser.Clone()
+
+		if err != nil {
+			releaser.Cleanup()
+			continue
+		}
+
+		_, err = releaser.Release()
+
+		if err != nil {
+			releaser.Cleanup()
+			continue
+		}
+
+		releaser.Cleanup()
 	}
-	fmt.Println("Done receiving!")
 }
