b2f1e3871ee8d723373b07f5e8982eb4c2ee6596
Clivern <hello@clivern.com>
2019-06-02 01:02:15 +0200

    add datastore skelton

--- internal/app/module/datastore.go
+++ internal/app/module/datastore.go
@@ -7,3 +7,43 @@ package module
 // DataStore struct
 type DataStore struct {
 }
+
+// Migrate migrates the datastore tables
+func (d *DataStore) Migrate() (bool, error) {
+	return true, nil
+}
+
+// Truncate truncates the datastore tables
+func (d *DataStore) Truncate() (bool, error) {
+	return true, nil
+}
+
+// StoreRelease stores the release data
+func (d *DataStore) StoreRelease(release Release) (bool, error) {
+	return true, nil
+}
+
+// DeleteReleaseByUUID deletes a release data by uuid
+func (d *DataStore) DeleteReleaseByUUID(uuid string) (bool, error) {
+	return true, nil
+}
+
+// DeleteReleaseByURL deletes a release data by url
+func (d *DataStore) DeleteReleaseByURL(url string) (bool, error) {
+	return true, nil
+}
+
+// GetReleaseByUUID gets a release data by uuid
+func (d *DataStore) GetReleaseByUUID(uuid string) (*Release, error) {
+	return &Release{}, nil
+}
+
+// GetReleaseByURL gets a release data by url
+func (d *DataStore) GetReleaseByURL(url string) (*Release, error) {
+	return &Release{}, nil
+}
+
+// GetReleases gets a list of releases
+func (d *DataStore) GetReleases(order string) ([]Release, error) {
+	return []Release{}, nil
+}
