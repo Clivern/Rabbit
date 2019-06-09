8ad6b8fbbff60bfa60035fff80fa475aa57cf86e
Clivern <hello@clivern.com>
2019-06-09 22:04:35 +0200

    add bitbucket push event

---
+++ pkg/bitbucket_events.go
@@ -0,0 +1,170 @@
+// Copyright 2019 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package pkg
+
+import (
+	"encoding/json"
+	"time"
+)
+
+// PushEvent when a new commit pushed or branch, tag got created
+type PushEvent struct {
+	Push struct {
+		Changes []struct {
+			Forced bool        `json:"forced"`
+			Old    interface{} `json:"old"`
+			Links  struct {
+				Commits struct {
+					Href string `json:"href"`
+				} `json:"commits"`
+			} `json:"links"`
+			Created   bool `json:"created"`
+			Truncated bool `json:"truncated"`
+			Closed    bool `json:"closed"`
+			New       struct {
+				Name  string `json:"name"`
+				Links struct {
+					Commits struct {
+						Href string `json:"href"`
+					} `json:"commits"`
+					Self struct {
+						Href string `json:"href"`
+					} `json:"self"`
+					HTML struct {
+						Href string `json:"href"`
+					} `json:"html"`
+				} `json:"links"`
+				Tagger  interface{} `json:"tagger"`
+				Date    interface{} `json:"date"`
+				Message interface{} `json:"message"`
+				Type    string      `json:"type"`
+				Target  struct {
+					Rendered struct {
+					} `json:"rendered"`
+					Hash  string `json:"hash"`
+					Links struct {
+						Self struct {
+							Href string `json:"href"`
+						} `json:"self"`
+						HTML struct {
+							Href string `json:"href"`
+						} `json:"html"`
+					} `json:"links"`
+					Author struct {
+						Raw  string `json:"raw"`
+						Type string `json:"type"`
+						User struct {
+							Username    string `json:"username"`
+							DisplayName string `json:"display_name"`
+							UUID        string `json:"uuid"`
+							Links       struct {
+								Self struct {
+									Href string `json:"href"`
+								} `json:"self"`
+								HTML struct {
+									Href string `json:"href"`
+								} `json:"html"`
+								Avatar struct {
+									Href string `json:"href"`
+								} `json:"avatar"`
+							} `json:"links"`
+							Nickname  string `json:"nickname"`
+							Type      string `json:"type"`
+							AccountID string `json:"account_id"`
+						} `json:"user"`
+					} `json:"author"`
+					Summary struct {
+						Raw    string `json:"raw"`
+						Markup string `json:"markup"`
+						HTML   string `json:"html"`
+						Type   string `json:"type"`
+					} `json:"summary"`
+					Parents    []interface{} `json:"parents"`
+					Date       time.Time     `json:"date"`
+					Message    string        `json:"message"`
+					Type       string        `json:"type"`
+					Properties struct {
+					} `json:"properties"`
+				} `json:"target"`
+			} `json:"new"`
+		} `json:"changes"`
+	} `json:"push"`
+	Actor struct {
+		Username    string `json:"username"`
+		DisplayName string `json:"display_name"`
+		UUID        string `json:"uuid"`
+		Links       struct {
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+			HTML struct {
+				Href string `json:"href"`
+			} `json:"html"`
+			Avatar struct {
+				Href string `json:"href"`
+			} `json:"avatar"`
+		} `json:"links"`
+		Nickname  string `json:"nickname"`
+		Type      string `json:"type"`
+		AccountID string `json:"account_id"`
+	} `json:"actor"`
+	Repository struct {
+		Scm     string `json:"scm"`
+		Website string `json:"website"`
+		Name    string `json:"name"`
+		Links   struct {
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+			HTML struct {
+				Href string `json:"href"`
+			} `json:"html"`
+			Avatar struct {
+				Href string `json:"href"`
+			} `json:"avatar"`
+		} `json:"links"`
+		FullName string `json:"full_name"`
+		Owner    struct {
+			Username    string `json:"username"`
+			DisplayName string `json:"display_name"`
+			UUID        string `json:"uuid"`
+			Links       struct {
+				Self struct {
+					Href string `json:"href"`
+				} `json:"self"`
+				HTML struct {
+					Href string `json:"href"`
+				} `json:"html"`
+				Avatar struct {
+					Href string `json:"href"`
+				} `json:"avatar"`
+			} `json:"links"`
+			Nickname  string `json:"nickname"`
+			Type      string `json:"type"`
+			AccountID string `json:"account_id"`
+		} `json:"owner"`
+		Type      string `json:"type"`
+		IsPrivate bool   `json:"is_private"`
+		UUID      string `json:"uuid"`
+	} `json:"repository"`
+}
+
+// LoadFromJSON update object from json
+func (e *PushEvent) LoadFromJSON(data []byte) (bool, error) {
+	err := json.Unmarshal(data, &e)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// ConvertToJSON convert object to json
+func (e *PushEvent) ConvertToJSON() (string, error) {
+	data, err := json.Marshal(&e)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
