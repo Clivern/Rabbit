446612c35e12c6b4323c0c7f76b27bbf1bb47120
Clivern <hello@clivern.com>
2019-06-21 20:01:56 +0200

    remove code duplication

--- internal/app/controller/github_listener.go
+++ internal/app/controller/github_listener.go
@@ -71,175 +71,50 @@ func GithubListener(c *gin.Context, messages chan<- string) {
 		return
 	}
 
-	// Create event received
-	if eventName == "create" {
-		var createEvent pkg.CreateEvent
-		createEvent.LoadFromJSON(rawBody)
+	var createEvent pkg.CreateEvent
+	createEvent.LoadFromJSON(rawBody)
 
+	logger.Info(fmt.Sprintf(
+		`Github event %s received`,
+		eventName,
+	), zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")))
+
+	if createEvent.RefType != "tag" {
 		logger.Info(fmt.Sprintf(
-			`Github event %s received`,
-			eventName,
+			`Github create event received but RefType is %s not tag`,
+			createEvent.RefType,
 		), zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")))
 
-		if createEvent.RefType != "tag" {
-			logger.Info(fmt.Sprintf(
-				`Github create event received but RefType is %s not tag`,
-				createEvent.RefType,
-			), zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")))
+		c.JSON(http.StatusOK, gin.H{
+			"status": "Nice!",
+		})
+		return
+	}
 
-			c.JSON(http.StatusOK, gin.H{
-				"status": "Nice!",
-			})
-			return
-		}
+	href := strings.ReplaceAll(
+		viper.GetString("integrations.github.https_format"),
+		"[.RepoFullName]",
+		createEvent.Repository.FullName,
+	)
 
-		href := strings.ReplaceAll(
-			viper.GetString("integrations.github.https_format"),
+	if viper.GetString("integrations.github.clone_with") == "ssh" {
+		href = strings.ReplaceAll(
+			viper.GetString("integrations.github.ssh_format"),
 			"[.RepoFullName]",
 			createEvent.Repository.FullName,
 		)
-
-		if viper.GetString("integrations.github.clone_with") == "ssh" {
-			href = strings.ReplaceAll(
-				viper.GetString("integrations.github.ssh_format"),
-				"[.RepoFullName]",
-				createEvent.Repository.FullName,
-			)
-		}
-
-		releaseRequest := model.ReleaseRequest{
-			Name:    createEvent.Repository.Name,
-			URL:     href,
-			Version: createEvent.Ref,
-		}
-
-		validate := pkg.Validator{}
-
-		if validate.IsEmpty(releaseRequest.Name) {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"status": "error",
-				"error":  "Repository name is required",
-			})
-			return
-		}
-
-		if validate.IsEmpty(releaseRequest.URL) {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"status": "error",
-				"error":  "Repository url is required",
-			})
-			return
-		}
-
-		if validate.IsEmpty(releaseRequest.Version) {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"status": "error",
-				"error":  "Repository version is required",
-			})
-			return
-		}
-
-		requestBody, err := releaseRequest.ConvertToJSON()
-
-		if err != nil {
-			logger.Error(fmt.Sprintf(
-				`Error while converting request body to json %s`,
-				err.Error(),
-			), zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")))
-
-			c.JSON(http.StatusBadRequest, gin.H{
-				"status": "error",
-				"error":  "Invalid request",
-			})
-			return
-		}
-
-		if viper.GetString("broker.driver") == "redis" {
-			driver := hippo.NewRedisDriver(
-				viper.GetString("redis.addr"),
-				viper.GetString("redis.password"),
-				viper.GetInt("redis.db"),
-			)
-
-			// connect to redis server
-			ok, err = driver.Connect()
-
-			if err != nil {
-				logger.Error(fmt.Sprintf(
-					`Unable to connect to redis server %s`,
-					err.Error(),
-				), zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")))
-
-				c.JSON(http.StatusServiceUnavailable, gin.H{
-					"status": "error",
-					"error":  "Internal server error",
-				})
-				return
-			}
-
-			if !ok {
-				logger.Error(
-					`Unable to connect to redis server`,
-					zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")),
-				)
-
-				c.JSON(http.StatusServiceUnavailable, gin.H{
-					"status": "error",
-					"error":  "Internal server error",
-				})
-				return
-			}
-
-			// ping check
-			ok, err = driver.Ping()
-
-			if err != nil {
-				logger.Error(fmt.Sprintf(
-					`Unable to ping redis server %s`,
-					err.Error(),
-				), zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")))
-
-				c.JSON(http.StatusServiceUnavailable, gin.H{
-					"status": "error",
-					"error":  "Internal server error",
-				})
-				return
-			}
-
-			if !ok {
-				logger.Error(
-					`Unable to ping redis server`,
-					zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")),
-				)
-
-				c.JSON(http.StatusServiceUnavailable, gin.H{
-					"status": "error",
-					"error":  "Internal server error",
-				})
-				return
-			}
-
-			logger.Info(fmt.Sprintf(
-				`Send request [%s] to workers`,
-				requestBody,
-			), zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")))
-
-			driver.Publish(
-				viper.GetString("broker.redis.channel"),
-				requestBody,
-			)
-		} else {
-			logger.Info(fmt.Sprintf(
-				`Send request [%s] to workers`,
-				requestBody,
-			), zap.String("CorrelationID", c.Request.Header.Get("X-Correlation-ID")))
-
-			messages <- requestBody
-		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"status": "Nice!",
-	})
+	releaseRequest := model.ReleaseRequest{
+		Name:    createEvent.Repository.Name,
+		URL:     href,
+		Version: createEvent.Ref,
+	}
 
+	passToWorker(
+		c,
+		messages,
+		logger,
+		releaseRequest,
+	)
 }
